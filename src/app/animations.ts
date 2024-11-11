00f5daed109b087ed137511f7635e91e43d09eb9
DouglasIde <douglasymide@gmail.com>
2024-11-11 17:49:56 -0300

    Feat: Creating the animation to inform the user whether the required fields are valid

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -125,9 +125,17 @@ export const flyInOutTrigger = trigger('flyInOut', [
 export const shakeTrigger = trigger('shakeAnimation', [
   transition('* => *', [
     query('input.ng-invalid:focus, select.ng-invalid:focus', [
-      animate('0.5s', style({
-        border: '6px solid red'
-      }))
+      animate('0.5s', keyframes([
+        style({ border: '2px solid red'}),
+        style({ transform: 'translateX(-10px)'}),
+        style({ transform: 'translateX(10px)'}),
+        style({ transform: 'translateX(-10px)'}),
+        style({ transform: 'translateX(10px)'}),
+        style({ transform: 'translateX(-10px)'}),
+        style({ transform: 'translateX(10px)'}),
+        style({ transform: 'translateX(-10px)'}),
+        style({ transform: 'translateX(0px)'})
+      ]))
     ])
   ])
 ])
